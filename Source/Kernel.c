a0571c97d97a32d1b5cca4dab69f8224527a103f
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2019-11-27 23:22:12 -0500

    Initial commit

---
+++ Source/Kernel.c
@@ -0,0 +1,4 @@
+void KMain(void)
+{
+    for(;;);
+}
---
+++ setup.bat
@@ -0,0 +1,7 @@
+SET "mingw=C:\MinGW\bin"
+SET "gcc=C:\Cross\ghost-i686-elf-tools\bin"
+SET "qemu=C:\Program Files\qemu"
+
+SET "PATH=%mingw%;%gcc%;%qemu%;%PATH%"
+
+meson Build
