51fc450833a71b5738f24ec0384f1500b1e48728
Elliot Brown <apachehaisley@gmail.com>
2019-11-30 21:54:52 -0500

    round 500

---
+++ kernel.c
@@ -0,0 +1,6 @@
+#include "types.h"
+
+void kernel_main(void)
+{
+    
+}
---
+++ types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long long i64;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+#endif
