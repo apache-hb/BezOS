cc54164aa92155d2529f6d4e081fc5748db81cf8
Elliot Brown <apachehaisley@gmail.com>
2019-12-06 11:32:56 -0500

    make the thing compile
    qemu doesnt like it though

---
+++ arch/x86_64/boot.cpp
@@ -0,0 +1,9 @@
+extern "C" void _kernel_bsp(void)
+{
+
+}
+
+extern "C" void _kernel_ap(void)
+{
+    
+}
--- etc/config.mk
+++ etc/config.mk
@@ -7,7 +7,18 @@ BUILD = $(ROOT)/build/$(TARGET)
 BIN = $(BUILD)/bin
 
 #magical C++ flags
-CXXFLAGS = -O3 -std=c++17 -mno-mmx -mno-sse -mno-sse2 -march=native -ffreestanding -nostdlib -no-pie -fno-PIC -mcmodel=kernel -mno-red-zone
+CXXFLAGS = 	-O3 \
+			-std=c++17 \
+			-mno-mmx \
+			-mno-sse \
+			-mno-sse2 \
+			-march=native \
+			-ffreestanding \
+			-nostdlib \
+			-mcmodel=kernel \
+			-mno-red-zone \
+			-fno-exceptions \
+			-fno-rtti
 
 LDFLAGS = -nostdlib -lgcc
 
