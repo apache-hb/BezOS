405299170e36ac82d47b58c3478efb63e4ab0afa
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-01-23 01:17:39 -0500

    paging stuff

---
+++ src/kernel/mm/paging.c
@@ -0,0 +1,11 @@
+#include "paging.h"
+
+void paging_init(void* top_page)
+{
+    
+}
+
+void* page_alloc(u64 num, page_size_t size)
+{
+
+}
--- src/kernel/mm/paging.h
+++ src/kernel/mm/paging.h
@@ -4,19 +4,21 @@
 #include "common/types.h"
 
 typedef enum {
-    // read only memory
-    // a page can always be read from
-    read = 0,
-    
-    // writeable memory, implies read as well
-    // bit 1 is the write enable bit
-    write = (1 << 1),
+    page_size_4kb = 1,
+    page_size_2mb = 2,
+    // this might be pointless to implement
+    page_size_4mb = 3,
+    page_size_1gb = 4,
+    // this might not be supported yet
+    page_size_512gb = 5,
+} page_size_t;
 
-    // executable memory
-    // if NX (bit 63) is 1 then the page cannot be executed
-    exec = (1 << 63)
-} paging_access;
+// initialize paging
+// @top_page the initial pml4, used for setup
+void paging_init(void* top_page);
 
-typedef u64 page_access_t;
-
+// allocate n pages from memory
+// @num number of pages to allocate
+// @size size of each page allocated
+void* page_alloc(u64 num, page_size_t size);
 #endif // MM_PAGING_H
