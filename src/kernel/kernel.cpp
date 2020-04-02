13619fa06365af1c0f6e2eb5a11fb59fc8bca5cf
Elliot Haisley <35050275+Apache-HB@users.noreply.github.com>
2020-04-02 09:51:34 -0400

    apic and msr support

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -8,9 +8,6 @@ extern "C" void kmain(void)
     vga::init();
 
     idt::init();
-
-    // mm::init *must* be called before vesa::init due to the bootloaders 
-    // delivery of data
     mm::init();
 
     vga::puts("hello\n");
