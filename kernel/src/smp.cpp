a3f2c6ef0a2df6ba1535d6b201a056245ff4e694
Elliot <apachehaisley@gmail.com>
2024-12-29 18:04:53 -0500

    get smp blob building

--- kernel/src/smp.cpp
+++ kernel/src/smp.cpp
@@ -1,5 +1,14 @@
 #include "smp.hpp"
 
+#include <stdint.h>
+
+struct [[gnu::packed]] SmpInfo {
+    uint32_t pml4;
+    uint32_t stackBase;
+    uint32_t stackSize;
+    uint32_t entry;
+};
+
 extern "C" void KmSmpStartup(void) {
 
 }
