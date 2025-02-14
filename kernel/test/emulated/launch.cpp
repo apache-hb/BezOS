dedc5d45063756c27bbb2bc3c76bcb4a073d67b0
Elliot <apachehaisley@gmail.com>
2025-02-13 20:57:45 -0500

    more robust virt tests

--- kernel/test/emulated/launch.cpp
+++ kernel/test/emulated/launch.cpp
@@ -5,6 +5,7 @@
 
 #include <libvirt/libvirt.h>
 #include <libvirt/virterror.h>
+#include <random>
 
 using namespace std::literals;
 
@@ -17,21 +18,21 @@ static void ReplaceAll(std::string &str, const std::string &from, const std::str
 }
 
 static std::string VirtLastErrorString() {
-    virErrorPtr error = virGetLastError();
-    if (error == nullptr) {
+    const char *msg = virGetLastErrorMessage();
+    if (msg == nullptr) {
         return "No error";
     }
 
-    std::string message = error->message;
-    virFreeError(error);
+    return msg;
+}
 
-    return message;
+static std::string GetTestDomain(std::string id) {
+    return "BezOS-IntegrationTest-"s + id;
 }
 
 static constexpr const char *kDomainXml = R"(
 <domain type='qemu'>
-    <name>BezOS-IntegrationTest-Launch</name>
-    <uuid>9a19e02c-ea6d-11ef-8ed3-00155dd28020</uuid>
+    <name>$0</name>
     <memory unit='MiB'>128</memory>
     <vcpu placement='static'>4</vcpu>
     <os>
@@ -47,7 +48,7 @@ static constexpr const char *kDomainXml = R"(
     <devices>
         <disk type='file' device='cdrom'>
             <driver name='qemu' type='raw'/>
-            <source file='/tmp/bezos-limine.iso'/>
+            <source file='$1'/>
             <target dev='hda' bus='ide'/>
             <readonly/>
             <address type='drive' controller='0' bus='0' target='0' unit='0'/>
@@ -95,12 +96,43 @@ static std::string GetEnvElement(const char *name) {
     return std::string(value);
 }
 
+static void AssertDomainState(virDomainPtr ptr, int expected) {
+    int state;
+    int ret = virDomainGetState(ptr, &state, nullptr, 0);
+    ASSERT_EQ(ret, 0) << VirtLastErrorString();
+    ASSERT_EQ(state, expected);
+}
+
+static std::string CopyKernelIso() {
+    std::mt19937 rng(std::random_device{}());
+    std::string random = "/tmp/bezos-test-"s + std::to_string(rng()) + ".iso";
+    auto iso = GetEnvElement("MESON_PREFIX") + "/bezos-limine.iso";
+    std::filesystem::copy_file(iso, random, std::filesystem::copy_options::overwrite_existing);
+
+    return random;
+}
+
+static void KillDomain(virConnectPtr conn, const std::string &name) {
+    virDomainPtr domain = virDomainLookupByName(conn, name.c_str());
+    if (domain == nullptr) {
+        return;
+    }
+
+    int ret = virDomainDestroy(domain);
+    if (ret != 0) {
+        std::cerr << "Failed to destroy domain: " << VirtLastErrorString() << std::endl;
+    }
+
+    ret = virDomainUndefine(domain);
+    if (ret != 0) {
+        std::cerr << "Failed to undefine domain: " << VirtLastErrorString() << std::endl;
+    }
+}
+
 //
 // Create a domain using a kernel image, launch it, and then destroy it.
 //
 TEST(LaunchTest, Launch) {
-    ASSERT_TRUE(true);
-
     virConnectPtr conn = virConnectOpen("qemu:///system");
     ASSERT_NE(conn, nullptr) << VirtLastErrorString();
     defer { virConnectClose(conn); };
@@ -112,14 +144,64 @@ TEST(LaunchTest, Launch) {
     const char *hvType = virConnectGetType(conn);
     ASSERT_NE(hvType, nullptr) << VirtLastErrorString();
 
-    auto qemuSystemX86_64 = ShellExec("which qemu-system-x86_64");
-    ASSERT_NE(qemuSystemX86_64, "") << "qemu-system-x86_64 not found";
+    auto name = GetTestDomain("Launch");
 
-    auto iso = GetEnvElement("MESON_PREFIX") + "/bezos-limine.iso";
-    std::filesystem::copy_file(iso, "/tmp/bezos-limine.iso", std::filesystem::copy_options::overwrite_existing);
+    // If a domain already exists for this test, kill it.
+    KillDomain(conn, name);
 
-    virDomainPtr domain = virDomainDefineXMLFlags(conn, kDomainXml, VIR_DOMAIN_DEFINE_VALIDATE);
-    ASSERT_NE(domain, nullptr) << VirtLastErrorString() << kDomainXml;
+    auto iso = CopyKernelIso();
+    std::string xml = kDomainXml;
+    ReplaceAll(xml, "$0", name);
+    ReplaceAll(xml, "$1", iso);
+
+    virDomainPtr domain = virDomainDefineXMLFlags(conn, xml.c_str(), VIR_DOMAIN_DEFINE_VALIDATE);
+    ASSERT_NE(domain, nullptr) << VirtLastErrorString() << xml;
+    defer { virDomainFree(domain); };
+
+    int ret = virDomainCreate(domain);
+    ASSERT_EQ(ret, 0) << VirtLastErrorString();
+
+    virDomainInfo info;
+    ret = virDomainGetInfo(domain, &info);
+    ASSERT_EQ(ret, 0) << VirtLastErrorString();
+
+    AssertDomainState(domain, VIR_DOMAIN_RUNNING);
+
+    ret = virDomainDestroy(domain);
+    ASSERT_EQ(ret, 0) << VirtLastErrorString();
+}
+
+static void SpamInjectNmi(virDomainPtr domain, int count) {
+    for (int i = 0; i < count; i++) {
+        int ret = virDomainInjectNMI(domain, 0);
+        ASSERT_EQ(ret, 0) << VirtLastErrorString();
+    }
+}
+
+TEST(LaunchTest, InjectNmi) {
+    virConnectPtr conn = virConnectOpen("qemu:///system");
+    ASSERT_NE(conn, nullptr) << VirtLastErrorString();
+    defer { virConnectClose(conn); };
+
+    char *uri = virConnectGetURI(conn);
+    ASSERT_NE(uri, nullptr) << VirtLastErrorString();
+    defer { free(uri); };
+
+    const char *hvType = virConnectGetType(conn);
+    ASSERT_NE(hvType, nullptr) << VirtLastErrorString();
+
+    auto name = GetTestDomain("InjectNmi");
+
+    // If a domain already exists for this test, kill it.
+    KillDomain(conn, name);
+
+    auto iso = CopyKernelIso();
+    std::string xml = kDomainXml;
+    ReplaceAll(xml, "$0", name);
+    ReplaceAll(xml, "$1", iso);
+
+    virDomainPtr domain = virDomainDefineXMLFlags(conn, xml.c_str(), VIR_DOMAIN_DEFINE_VALIDATE);
+    ASSERT_NE(domain, nullptr) << VirtLastErrorString() << xml.c_str();
     defer { virDomainFree(domain); };
 
     int ret = virDomainCreate(domain);
@@ -133,11 +215,16 @@ TEST(LaunchTest, Launch) {
     ret = virDomainGetState(domain, &state, nullptr, 0);
     ASSERT_EQ(ret, 0) << VirtLastErrorString();
 
-    ASSERT_EQ(state, VIR_DOMAIN_RUNNING) << state;
+    AssertDomainState(domain, VIR_DOMAIN_RUNNING);
+
+    SpamInjectNmi(domain, 25);
+
+    std::this_thread::sleep_for(500ms);
+
+    SpamInjectNmi(domain, 25);
+
+    AssertDomainState(domain, VIR_DOMAIN_RUNNING);
 
     ret = virDomainDestroy(domain);
-    ASSERT_EQ(ret, 0) << VirtLastErrorString();
-
-    ret = virDomainUndefine(domain);
     ASSERT_EQ(ret, 0) << VirtLastErrorString();
 }
