249dc649966156360be4343bef1870a53867ead5
Elliot <apachehaisley@gmail.com>
2025-03-27 16:07:47 -0400

    add address space util functions

--- sources/kernel/include/memory/address_space.hpp
+++ sources/kernel/include/memory/address_space.hpp
@@ -7,6 +7,11 @@
 #include <type_traits>
 
 namespace km {
+    namespace detail {
+        template<typename T>
+        static constexpr PageFlags kDefaultPageFlags = std::is_const_v<T> ? PageFlags::eRead : PageFlags::eData;
+    }
+
     class AddressSpace {
         PageTables mTables;
         VmemAllocator mVmemAllocator;
@@ -25,26 +30,38 @@ namespace km {
 
         template<typename T> requires (std::is_standard_layout_v<T>)
         [[nodiscard]]
-        T *mapObject(MemoryRange range, MemoryType type = MemoryType::eWriteBack);
+        T *mapObject(MemoryRange range, PageFlags flags = detail::kDefaultPageFlags<T>, MemoryType type = MemoryType::eWriteBack) {
+            return (T*)map(range, flags, type);
+        }
 
         template<typename T> requires (std::is_standard_layout_v<T>)
         [[nodiscard]]
-        T *mapObject(PhysicalAddress paddr, MemoryType type = MemoryType::eWriteBack);
+        T *mapObject(PhysicalAddress paddr, PageFlags flags = detail::kDefaultPageFlags<T>, MemoryType type = MemoryType::eWriteBack) {
+            return mapObject<T>(MemoryRange::of(paddr, sizeof(T)), flags, type);
+        }
 
         template<typename T> requires (std::is_standard_layout_v<T>)
         [[nodiscard]]
-        const T *mapConst(MemoryRange range, MemoryType type = MemoryType::eWriteBack);
+        const T *mapConst(MemoryRange range, MemoryType type = MemoryType::eWriteBack) {
+            return (const T*)map(range, PageFlags::eRead, type);
+        }
 
         template<typename T> requires (std::is_standard_layout_v<T>)
         [[nodiscard]]
-        const T *mapConst(PhysicalAddress paddr, MemoryType type = MemoryType::eWriteBack);
+        const T *mapConst(PhysicalAddress paddr, MemoryType type = MemoryType::eWriteBack) {
+            return mapConst<T>(MemoryRange::of(paddr, sizeof(T)), type);
+        }
 
         template<typename T> requires (std::is_standard_layout_v<T>)
         [[nodiscard]]
-        T *mapMmio(PhysicalAddress paddr, PageFlags flags = PageFlags::eData);
+        T *mapMmio(PhysicalAddress paddr, PageFlags flags = detail::kDefaultPageFlags<T>) {
+            return mapObject<T>(MemoryRange::of(paddr, sizeof(T)), flags, MemoryType::eUncached);
+        }
 
         template<typename T> requires (std::is_standard_layout_v<T>)
         [[nodiscard]]
-        T *mapMmio(MemoryRange range, PageFlags flags = PageFlags::eData);
+        T *mapMmio(MemoryRange range, PageFlags flags = detail::kDefaultPageFlags<T>) {
+            return mapObject<T>(range, flags, MemoryType::eUncached);
+        }
     };
 }
