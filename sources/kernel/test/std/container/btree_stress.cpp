cfa5591d7f3a26e4b043bb37e2857e92912d584a
Elliot <apachehaisley@gmail.com>
2025-06-28 13:26:58 -0400

    dont use gtest for stress tests

--- sources/kernel/test/std/container/btree_stress.cpp
+++ sources/kernel/test/std/container/btree_stress.cpp
@@ -2,40 +2,43 @@
 #   undef NDEBUG
 #endif
 
-#include "std/container/btree.hpp"
-
 #include <absl/container/btree_map.h>
+#include "std/container/btree.hpp"
 #include <random>
 
 int main() {
-    static constexpr size_t kElementCount = 1000'000;
     sm::BTreeMap<int, int> tree;
     absl::btree_map<int, int> baseline;
 
-    std::mt19937 mt(0x1234);
-    std::uniform_int_distribution<int> dist(INT_MIN, INT_MAX);
+    std::mt19937 mtValues(0x1234);
+    std::mt19937 mtActions(0x5678);
+    std::uniform_int_distribution<int> distValues(INT_MIN, INT_MAX);
+    std::uniform_int_distribution<int> distActions(0, 3); // 0 = insert, 1 = insert, 2 = find, 3 = remove
 
-    for (size_t i = 0; i < kElementCount; i++) {
-        int key = dist(mt);
-        tree.insert(key, i);
-        baseline[key] = i;
-    }
-
-    for (auto& [key, value] : baseline) {
-        auto it = tree.find(key);
-        if (it == tree.end()) {
-            tree.dump();
-            assert(false && "Key not found in BTreeMap");
+    static constexpr size_t kFuzzIterations = 100000;
+    for (size_t i = 0; i < kFuzzIterations; i++) {
+        int key = distValues(mtValues);
+        int value = distValues(mtValues);
+        int action = distActions(mtActions);
+        if (action == 0 || action == 1) { // Insert
+            tree.insert(key, value);
+            baseline[key] = value;
+            assert(tree.contains(key));
+        } else if (action == 2) { // Find
+            auto it = tree.find(key);
+            if (it != tree.end()) {
+                auto [foundKey, foundValue] = *it;
+                assert(foundKey == key);
+                assert(baseline.contains(key));
+            } else {
+                assert(!tree.contains(key));
+                assert(!baseline.contains(key));
+            }
+        } else if (action == 3) { // Remove
+            tree.remove(key);
+            baseline.erase(key);
+            assert(!tree.contains(key));
+            assert(!baseline.contains(key));
         }
-
-        auto [foundKey, foundValue] = *it;
-        if (foundKey != key || foundValue != value) {
-            tree.dump();
-            assert(false && "Key or value mismatch in BTreeMap");
-        }
-
-        assert(tree.contains(key) && "Key not found in BTreeMap after insertion");
-        tree.erase(it);
-        assert(!tree.contains(key) && "Key found in BTreeMap after removal");
     }
 }
