1390325e8af69c908b4959a10d10c06743993a48
Elliot <apachehaisley@gmail.com>
2025-07-05 10:42:14 -0400

    more benchmarks

--- sources/kernel/test/std/container/btree_bench.cpp
+++ sources/kernel/test/std/container/btree_bench.cpp
@@ -94,3 +94,95 @@ static void BM_BezosBtreeFind(benchmark::State& state) {
 
 BENCHMARK(BM_BezosBtreeFind)
     ->Range(32, 8 << 14);
+
+static void BM_AbseilBtreeErase(benchmark::State& state) {
+    absl::btree_map<int, int> btree;
+    std::unique_ptr<int[]> keys(new int[state.range(0)]);
+    std::mt19937 mt(0x1234);
+    std::uniform_int_distribution<int> dist(0, INT_MAX);
+    for (int64_t i = 0; i < state.range(0); i++) {
+        keys[i] = dist(mt);
+    }
+
+    std::shuffle(keys.get(), keys.get() + state.range(0), mt);
+
+    for (auto _ : state) {
+        state.PauseTiming();
+        for (int64_t i = 0; i < state.range(0); i++) {
+            btree.insert({keys[i], i});
+        }
+        state.ResumeTiming();
+
+        for (int64_t i = 0; i < state.range(0); i++) {
+            btree.erase(keys[i]);
+        }
+    }
+}
+
+BENCHMARK(BM_AbseilBtreeErase)
+    ->Range(32, 8 << 14);
+
+static void BM_BezosBtreeErase(benchmark::State& state) {
+    sm::BTreeMap<int, int> btree;
+    std::unique_ptr<int[]> keys(new int[state.range(0)]);
+    std::mt19937 mt(0x1234);
+    std::uniform_int_distribution<int> dist(0, INT_MAX);
+    for (int64_t i = 0; i < state.range(0); i++) {
+        keys[i] = dist(mt);
+    }
+
+    std::shuffle(keys.get(), keys.get() + state.range(0), mt);
+
+    for (auto _ : state) {
+        state.PauseTiming();
+        for (int64_t i = 0; i < state.range(0); i++) {
+            btree.insert(keys[i], i);
+        }
+        state.ResumeTiming();
+
+        for (int64_t i = 0; i < state.range(0); i++) {
+            btree.remove(keys[i]);
+        }
+    }
+}
+
+BENCHMARK(BM_BezosBtreeErase)
+    ->Range(32, 8 << 14);
+
+static void BM_AbseilBtreeIterate(benchmark::State& state) {
+    absl::btree_map<int, int> btree;
+    std::mt19937 mt(0x1234);
+    std::uniform_int_distribution<int> dist(0, INT_MAX);
+    for (int64_t i = 0; i < state.range(0); i++) {
+        btree.insert({dist(mt), i});
+    }
+
+    for (auto _ : state) {
+        for (const auto& [key, value] : btree) {
+            benchmark::DoNotOptimize(auto(key));
+            benchmark::DoNotOptimize(auto(value));
+        }
+    }
+}
+
+BENCHMARK(BM_AbseilBtreeIterate)
+    ->Range(32, 8 << 14);
+
+static void BM_BezosBtreeIterate(benchmark::State& state) {
+    sm::BTreeMap<int, int> btree;
+    std::mt19937 mt(0x1234);
+    std::uniform_int_distribution<int> dist(0, INT_MAX);
+    for (int64_t i = 0; i < state.range(0); i++) {
+        btree.insert(dist(mt), i);
+    }
+
+    for (auto _ : state) {
+        for (const auto& [key, value] : btree) {
+            benchmark::DoNotOptimize(auto(key));
+            benchmark::DoNotOptimize(auto(value));
+        }
+    }
+}
+
+BENCHMARK(BM_BezosBtreeIterate)
+    ->Range(32, 8 << 14);
